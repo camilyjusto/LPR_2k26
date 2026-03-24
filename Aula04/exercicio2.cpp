b020e816576aaf7d6bd100652739146016d8b23b
camilyjusto <camilyjusto.oficial@gmail.com>
2026-03-24 08:23:38 -0300

    exercicio 2 melhorado

--- Aula04/exercicio2.cpp
+++ Aula04/exercicio2.cpp
@@ -10,15 +10,14 @@ int main()
     cout << "Digite o primeiro número inteiro:" << "\n";
     cin >> n2;
     
-    if (n1 % n2 == 0){
-
-        cout << "Os números " << n1 << " e " << n2 << " são multiplos.";
-    }
-    else if (n2 % n1 == 0){
+    if (n1 % n2 == 0 || n2 % n1 == 0){
 
         cout << "Os números " << n1 << " e " << n2 << " são multiplos.";
     }
     else{
         cout << "Os números " << n1 << " e " << n2 << " não são multiplos.";
     }
+
+    system("pause")
+    return 0;
 }
